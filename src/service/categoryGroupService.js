816eb42ce7957b508b8a6bfb389d1c0addb9c961
kamlesh kumar <coderkamlesg68@gmail.com>
2025-12-01 02:16:34 +0530

    github actions added

--- src/service/categoryGroupService.js
+++ src/service/categoryGroupService.js
@@ -2,22 +2,22 @@ import apiClient from '@/lib/axios';
 
 export const categoryGroupService = {
   fetchAll: async () => {
-    const res = await apiClient.get('/api/categoryGroup/fetchAll');
+    const res = await apiClient.get('/categoryGroup/fetchAll');
     return res.data;
   },
 
   create: async payload => {
-    const res = await apiClient.post('/api/categoryGroup', payload);
+    const res = await apiClient.post('/categoryGroup', payload);
     return res.data;
   },
 
   update: async (id, payload) => {
-    const res = await apiClient.put(`/api/categoryGroup/${id}`, payload);
+    const res = await apiClient.put(`/categoryGroup/${id}`, payload);
     return res.data;
   },
 
   remove: async id => {
-    const res = await apiClient.delete(`/api/categoryGroup/${id}`);
+    const res = await apiClient.delete(`/categoryGroup/${id}`);
     return res.data;
   },
 };
